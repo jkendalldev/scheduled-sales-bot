5392f1d5f5ce91d36abff5e9c2b1edcc90f55778
Jason <jkendall1975@gmail.com>
2022-02-06 15:03:59 -0500

    add date time

--- scheduled-job.js
+++ scheduled-job.js
@@ -1,5 +1,7 @@
 function sayHello() {
     console.log('Hello from scheduled-sales-bot! Scheduled Run 2 !');
+    // prints date & time in YYYY-MM-DD HH:MM:SS format
+    console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
 }
 
 sayHello();
